839bf1db2e17e4b5f7a814f6dbf1ca7f1ed76095
denbarabraza <denis.bareischev@gmail.com>
2022-10-17 22:39:38 +0300

    hw_mt_monday_01

---
+++ src/NewComponent2.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+type StudentsType = {
+    id: number,
+    name: string,
+    age: number,
+}
+
+type NewComponentType = {
+    students: Array<StudentsType>,
+}
+
+export const NewComponent1: React.FC<NewComponentType> = (props) => {
+    return (
+        <ol>
+            {props.students.map((el) => {
+                debugger
+                return (
+                    <li key={el.id}>
+                        <span>{el.name}</span>
+                        <span> age: {el.age}</span>
+                    </li>
+                )
+            })}
+        </ol>
+    )
+}
